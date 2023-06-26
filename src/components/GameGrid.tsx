ed2c31fc70c6edaa29062deba9966ef827c3781a
MH <mh9898@gmail.com>
2023-06-26 11:57:36 +0300

    add game card

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,16 +1,17 @@
-import { Text } from "@chakra-ui/react";
+import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
+import GameCard from "./GameCard";
 
 const GameGrid = () => {
   const { games, errorMessage } = useGames();
   return (
     <>
       {errorMessage && <Text>{errorMessage} </Text>}
-      <ul>
+      <SimpleGrid columns={3} spacing={10}>
         {games.map((game) => (
-          <li key={game.id}>{game.name}</li>
+          <GameCard key={game.id} game={game} />
         ))}
-      </ul>
+      </SimpleGrid>
     </>
   );
 };
