6e3fad121dd5e248a8e23556ea7054d0557be85c
mharari <mharari@varcode.com>
2023-06-28 14:51:11 +0300

    Testing with Scan Results

---
+++ src/components/TempModeResults.tsx
@@ -0,0 +1,51 @@
+
+import { Table } from "@mantine/core";
+import { MoodSmile, MoodEmpty, MoodOff, MoodSad, CircleOff } from 'tabler-icons-react';
+
+const TempModeResults = () => {
+    return (
+        <Table cellPadding="10px">
+            <thead>
+            <tr>
+                <th> <CircleOff //MoodOff
+                    size={48}
+                    strokeWidth={2}
+                    color={'#2E2EFF'}
+                />
+                </th>
+                <th> <MoodSmile
+                    size={48}
+                    strokeWidth={2}
+                    color={'#8ad279'}
+                />
+                </th>
+                <th>
+                    <MoodEmpty
+                        size={48}
+                        strokeWidth={2}
+                        color={'#f59f00'}
+                    />
+                </th>
+                <th>
+                    <MoodSad
+                        size={48}
+                        strokeWidth={2}
+                        color={'#bf4040'}
+                    />
+                </th>
+            </tr>
+            <tr>
+                <th>OFF ~ QC1</th>
+                <th>Good ~ QC2</th>
+                <th>OK ~ QC3</th>
+                <th>Bad ~ QC4</th>
+            </tr>
+
+
+            </thead>
+
+        </Table>
+    );
+};
+
+export default TempModeResults;
---
+++ src/components/TemptResultBar.tsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import { Progress } from '@mantine/core';
+import {Box} from "@chakra-ui/react";
+
+const TemptResultBar = () => {
+    return (
+        <Box width="800px" padding={10}>
+            ALL
+            <Progress
+                mt="md"
+                size={24}
+                radius="sm"
+                sections={[
+                    { value: 25, color: 'gray', label: 'OFF - QC1' },
+                    { value: 25, color: 'green', label: 'Good ~ QC2' },
+                    { value: 25, color: 'yellow', label: 'OK ~ QC3' },
+                    { value: 25, color: 'red', label: 'Bad ~ QC4' },
+                ]}
+            />
+
+            QC1
+            <Progress
+                mt="md"
+                size={24}
+                radius="sm"
+                sections={[
+                    { value: 25, color: 'gray', label: 'OFF - QC1' },
+                    { value: 0, color: 'green', label: 'Good ~ QC2' },
+                    { value: 0, color: 'yellow', label: 'OK ~ QC3' },
+                    { value: 0, color: 'red', label: 'Bad ~ QC4' },
+                    { value: 75, color: 'black', label: 'Take Out Green sticker & Scan' },
+                ]}
+            />
+
+            QC2
+            <Progress
+                mt="md"
+                size={24}
+                radius="sm"
+                sections={[
+                    { value: 0, color: 'gary', label: 'OFF - QC1' },
+                    { value: 25, color: 'green', label: 'Good ~ QC2' },
+                    { value: 0, color: 'yellow', label: 'OK ~ QC3' },
+                    { value: 0, color: 'red', label: 'Bad ~ QC4' },
+                    { value: 75, color: 'black', label: 'Product within target temperature' },
+                ]}
+            />
+
+            QC3
+            <Progress
+                mt="md"
+                size={24}
+                radius="sm"
+                sections={[
+                    { value: 0, color: 'gary', label: 'OFF - QC1' },
+                    { value: 0, color: 'green', label: 'Good ~ QC2' },
+                    { value: 25, color: 'yellow', label: 'OK ~ QC3' },
+                    { value: 0, color: 'red', label: 'Bad ~ QC4' },
+                    { value: 75, color: 'black', label: 'Product was over temperature for 2-4 hours' },
+                ]}
+            />
+QC4
+            <Progress
+                mt="md"
+                size={24}
+                radius="sm"
+                sections={[
+                    { value: 0, color: 'gary', label: 'OFF - QC1' },
+                    { value: 0, color: 'green', label: 'Good ~ QC2' },
+                    { value: 0, color: 'yellow', label: 'OK ~ QC3' },
+                    { value: 25, color: 'red', label: 'Bad ~ QC4' },
+                    { value: 75, color: 'black', label: 'Product was over temperature for 4+ hours' },
+                ]}
+            />
+
+
+        </Box>
+    );
+};
+
+export default TemptResultBar;
